771a6edfc8a40a2a1875c7b6427b3cd86b4d7f3c
TT0057 <deep005>
2018-04-17 00:04:36 +0530

    using the pre hook to encrypt the password before saving it to db. 2> generating unique images for each user. 3>Comapre password method to compare the text and encrypted passwod

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+const crypto = require('crypto');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -10,4 +12,38 @@ const UserSchema = new Schema({
         tweet: {type: Schema.Types.ObjectId, ref: 'Tweet'}
     }]
 });
+
+UserSchema.pre('save', function(next){
+    var user = this;
+    if(!user.isModified('password'))
+        return next();
+    if(user.password){
+        bcrypt.genSalt(10, function(err, salt){
+            if(err)
+                return next(err);
+            bcrypt.hash(user.password, salt, null, function(err, hash){
+                if(err) 
+                    return next();
+                user.password = hash;
+                next(err);
+            });
+        });
+    }
+});
+
+//creating a method to generate unique pictures for each user
+UserSchema.methods.gravatar = function(size){
+    if(!size)
+        size = 200;
+    if(!this.email)
+        return 'https://gravatar.com/avatar/?s=' + size + '&d=retro';
+    var md5 = crypto.createHash('md5').update(this.email).digest('hex');
+    return 'https://gravatar.com/avatar/' + md5 + '?s=' + size + '&d=retro';
+};
+
+//A function to compare the text password and the encrypted password in the db
+UserSchema.methods.comparePassword = function(password){
+    return bcrypt.compareSync(password, this.password);
+}
+
 module.exports = mongoose.model('User', UserSchema);
